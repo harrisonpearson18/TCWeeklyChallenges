01afac6d5fd01fec9876f8bd34dd362083f7d5e9
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-07-14 16:43:42 -0500

    Week 2 Challenges

--- WeeklyChallenges/ChallengesSet02.cs
+++ WeeklyChallenges/ChallengesSet02.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace WeeklyChallenges
 {
@@ -7,52 +10,145 @@ namespace WeeklyChallenges
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            bool result = Char.IsLetter(c);
+            return result;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int number = vals.Length;
+
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            bool result = (number % 2 == 0) ? true : false;
+            return result;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            bool result = (num % 2 == 0) ? false : true;
+            return result;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (numbers.Count() != 0)
+                {
+                    double min = numbers.Min();
+                    double max = numbers.Max();
+                    double sum = min + max;
+                    return sum;
+                }
+                return 0;
+            }
+
+   
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str1.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (int item in numbers)
+                {
+                    sum += item;
+                }
+                return sum;
+            }
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (int item in numbers)
+                {
+                    if (item % 2 == 0)
+                    {
+                        sum += item;
+                    }
+                    else if (item == 0)
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (int item in numbers)
+                {
+                    sum += item;
+                }
+                if (sum % 2 == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long odds = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    odds++;
+                }
+            }
+         return odds;
         }
     }
 }
